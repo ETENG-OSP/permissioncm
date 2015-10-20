01e7983da5e301dba1b4ba18aa52d9d68112a0bf
Gen Liu <gg@linux-o4mm.site>
2015-10-21 01:01:04 +0800

    update controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,52 +1,59 @@
 var User = require('../models').User;
+var Permission = require('../models').Permission;
+var Role = require('../models').Role;
+
+function getInclude(req, Model) {
+  try {
+    var include = req.cm.param('_include');
+    if (Array.isArray(include)) {
+      throw new Error('include is not an array');
+    }
+    return include.map(function(name) {
+      return {
+        association: Model.associations[name]
+      };
+    });
+  } catch (err) {
+    return;
+  }
+}
 
 function getPermissions(req, res, next) {
-
   var userId = req.cm.param('id');
-
   var user = new User(userId);
-
   user
-    .getPermissions()
+    .getPermissions({
+      include: getInclude(req, Permission)
+    })
     .then(function(permissions) {
       res.json(permissions);
     })
     .catch(next);
-
 }
 
 function getRoles(req, res, next) {
-
   var userId = req.cm.param('id');
-
   var user = new User(userId);
-
-  console.log('=== get roles', userId);
-
   user
-    .getRoles()
+    .getRoles({
+      include: getInclude(req, Role)
+    })
     .then(function(roles) {
-      console.log('=== results', roles);
       res.json(roles);
     })
     .catch(next);
-
 }
 
 function setRoles(req, res, next) {
-
   var userId = req.cm.param('id');
   var roleIds = req.cm.param('data').roles;
-
   var user = new User(userId);
-
   user
     .setRoles(roleIds)
     .then(function() {
       res.json();
     })
     .catch(next);
-
 }
 
 exports.getPermissions = getPermissions;
